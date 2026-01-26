fb1df2c35c3e59499f2a43bf24d202957c2a8854
Toni Pham <139079071+tonidevvn@users.noreply.github.com>
2026-01-25 21:52:04 -0500

    feat: update Avatar component for displaying user images with optional animation.

--- client_nextjs/app/components/Avatar.tsx
+++ client_nextjs/app/components/Avatar.tsx
@@ -12,7 +12,7 @@ type AvatarType = 'avatar' | 'photo';
 const Avatar = ({ animate = true, type = 'photo', class: customClass = '' }: AvatarProps) => {
     return (
         <div className={`image-wrapper ${animate ? 'animate-float' : ''} ${customClass}`}>
-            <img src={type === 'avatar' ? myavatar : myphoto} alt='Avatar' className='w-full h-full object-cover rounded-full' />
+            <img src={type === 'avatar' ? myavatar : myphoto} alt='Avatar' className='w-full h-full object-cover rounded-full glow-accent opacity-95 dark:opacity-90' />
         </div>
     );
 };
